8d3b071af4a1e7d4d0c81cc0c38cdbe43d2436d3
1718077 <1718077@CS17077>
2018-04-15 11:42:04 +0900

    プロジェクト ファイルを追加します。

---
+++ AVXTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AVXTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
